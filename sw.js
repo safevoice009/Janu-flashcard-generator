3c9e3f758a94fa4999ecdc6998297ccda653771d
Panther009 <panthers009@proton.me>
2025-10-22 08:19:49 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+// Simple PWA cache
+const CACHE = 'occludex-v1';
+const ASSETS = [
+  '/', '/index.html', '/styles.css', '/script.js', '/manifest.json',
+  '/icons/icon-192.png', '/icons/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  if (url.origin === location.origin) {
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  }
+});
